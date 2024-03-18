a27de02684cb1bbeba7758ca91970258204e8b96
Rohan <chhabratanishq13@gmail.com>
2024-03-18 08:16:33 -0400

    Dockerfile

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const SERVER_PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/views/index.html');
@@ -10,6 +11,6 @@ app.get('/products', (req, res)=>{
 }
 );
 
-app.listen(3000,()=>{
-    console.log('sever 3000');
-});
+app.listen(SERVER_PORT, () => {
+    console.log(`Server running on port ${SERVER_PORT}`);
+  }); 
